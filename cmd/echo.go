d73d0d996d4334734fef0f67bebce8b0420d3251
Aulia Fikri <aulia.fikri101219@students.unila.ac.id>
2022-05-18 22:51:21 +0700

    Add input data your program

--- cmd/echo.go
+++ cmd/echo.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/aulfik/lcs/data"
@@ -38,18 +40,54 @@ func generateFile(licenseType string) {
 
 	defer f.Close()
 
+	consoleReader := bufio.NewReader(os.Stdin)
 	var licText string
 
+	nameQ := "Enter Your Name: <name of author>"
+	yearQ := "Enter Your Year License: <year>"
+	programQ := "Program name and idea: <one line to give the program's name and a brief idea of what it does.>"
+	programNameQ := "Name Program: <program>"
+
 	if licenseType == "MIT License" {
-		licText = data.GetMitText("2022", "Aulia")
+		fmt.Println(nameQ)
+		name, _ := consoleReader.ReadString('\n')
+		name = strings.TrimSuffix(name, "\n")
+		fmt.Println(yearQ)
+		year, _ := consoleReader.ReadString('\n')
+		year = strings.TrimSuffix(year, "\n")
+
+		licText = data.GetMitText(year, name)
 	}
 
 	if licenseType == "GNU AGPLv3" {
-		licText = data.GetGnuAgplv3Text("Program", "2022", "Aulia Fikri")
+		fmt.Println(programQ)
+		program, _ := consoleReader.ReadString('\n')
+		program = strings.TrimSuffix(program, "\n")
+		fmt.Println(nameQ)
+		name, _ := consoleReader.ReadString('\n')
+		name = strings.TrimSuffix(name, "\n")
+		fmt.Println(yearQ)
+		year, _ := consoleReader.ReadString('\n')
+		year = strings.TrimSuffix(year, "\n")
+
+		licText = data.GetGnuAgplv3Text(program, year, name)
 	}
 
 	if licenseType == "GNU GPLv3" {
-		licText = data.GetGnuGplv3Text("Program Well", "2022", "Aulia Fikri", "My Program")
+		fmt.Println(programNameQ)
+		programName, _ := consoleReader.ReadString('\n')
+		programName = strings.TrimSuffix(programName, "\n")
+		fmt.Println(programQ)
+		program, _ := consoleReader.ReadString('\n')
+		program = strings.TrimSuffix(program, "\n")
+		fmt.Println(nameQ)
+		name, _ := consoleReader.ReadString('\n')
+		name = strings.TrimSuffix(name, "\n")
+		fmt.Println(yearQ)
+		year, _ := consoleReader.ReadString('\n')
+		year = strings.TrimSuffix(year, "\n")
+
+		licText = data.GetGnuGplv3Text(program, year, name, programName)
 	}
 
 	if licenseType == "GNU LGPLv3" {
@@ -61,7 +99,14 @@ func generateFile(licenseType string) {
 	}
 
 	if licenseType == "Apache License 2.0" {
-		licText = data.GetApacheText("2022", "Aulia Fikri")
+		fmt.Println(nameQ)
+		name, _ := consoleReader.ReadString('\n')
+		name = strings.TrimSuffix(name, "\n")
+		fmt.Println(yearQ)
+		year, _ := consoleReader.ReadString('\n')
+		year = strings.TrimSuffix(year, "\n")
+
+		licText = data.GetApacheText(year, name)
 	}
 
 	if licenseType == "Boost Software License 1.0" {
